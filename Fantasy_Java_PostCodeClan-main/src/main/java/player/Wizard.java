3e0cee21b2c4a8123e2d6ea9ab86d27402b58440
Guilhermelsnunes <guilhermelsnunes@gmail.com>
2022-05-04 12:45:18 +0100

    updated version with warlock etc

---
+++ Fantasy_Java_PostCodeClan-main/src/main/java/player/Wizard.java
@@ -0,0 +1,17 @@
+package player;
+
+import magic.ISpell;
+import protects.IDefend;
+
+public class Wizard extends Mage{
+    public Wizard(String name, int healthPoints, ISpell spell, IDefend defend) {
+        super(name, healthPoints, spell, defend);
+    }
+
+}
+
+
+
+
+
+
